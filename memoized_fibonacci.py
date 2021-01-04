4aa9232c15356c3159329e3315270be9fbe5adaf
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-01-04 09:32:39 -0700

    added memoized fibonacci sequence code

---
+++ memoized_fibonacci.py
@@ -0,0 +1,21 @@
+from functools import lru_cache
+
+# Using tool to cache function values
+
+
+@lru_cache(maxsize=1000)
+def fibonacci(n):
+    if type(n) != int:
+        raise TypeError("n needs to be a positive int.")
+    if n < 0:
+        raise ValueError("n needs to be a postive int.")
+
+    if n <= 2:
+        return 1
+
+    return fibonacci(n - 1) + fibonacci(n - 2)
+
+
+if __name__ == '__main__':
+    for n in range(0, 101):
+        print(f"{n}: {fibonacci(n)}")
