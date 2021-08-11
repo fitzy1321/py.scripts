1b799b591dc048b6f45e4603f86b1326eed4eb06
Joe Fitzgibbons <joe.fitzgibbons@ruralsourcing.com>
2021-08-11 09:12:33 -0600

    dict operations

---
+++ dict_operations.py
@@ -0,0 +1,26 @@
+# noqa
+
+from pprint import pprint
+
+
+def main():  # noqa
+    a: dict = {
+        "some": "value",
+        "a_bool": True,
+        "this": "is",
+        "a_str": "this is a string",
+    }
+    b: dict = {
+        "a_str": "different string, yo",
+        "a_bool": False,
+        "b": 123123,
+        "c": 125551,
+    }
+    c: dict = {"asdasd": "Fasdfas", "ywekjf": "129sdhdf"}
+
+    z = a | b | c
+    pprint(z)
+
+
+if __name__ == "__main__":
+    main()
