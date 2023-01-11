8dd2380d5477f7b7f077c8a35ad7ac85fd190d41
fitzypop <32967490+fitzypop@users.noreply.github.com>
2023-01-10 21:57:48 -0700

    aiohttp mock update

--- test_aiohttp_mock.py
+++ test_aiohttp_mock.py
@@ -1,27 +1,58 @@
-from unittest.mock import MagicMock
+"""
+Mocking aiohttp async, nested, context managers for testing.
+
+Specifically, the request method.
+
+Run the following to get started
+pip install --upgrade aiohttp pytest pytest-asyncio
+pytest -rP "test_aiohttp_mock.py"
+"""
+import sys
+
+import pytest
+
+
+if sys.version_info < (3,8):
+    pytest.skip(
+        "must use 3.8 or greater",
+        allow_module_level=True,
+    )
+
+
+from unittest.mock import AsyncMock, Mock
 
 import aiohttp
-import pytest
-from asyncmock import AsyncMock
 
-# pytest -rP test_aiohttp_mock.py
+class ContextManagerMock:
+    async def __aenter__(self):
+        pass
+
+    async def __aexit__(self):
+        pass
+
+    def request(self):
+        pass
 
 
 @pytest.fixture
 def aiomock() -> tuple:
     response = AsyncMock()
     response.json.return_value = {"body": "asdf"}
-    response.raise_for_status = MagicMock(side_effect=aiohttp.ClientError)
+    response.raise_for_status = Mock(side_effect=aiohttp.ClientError)
 
-    session = AsyncMock()
-    session.request = MagicMock(return_value=response)
+    context = AsyncMock(ContextManagerMock)
+    context.request.return_value.__aenter__.return_value = response
 
-    return session, response
+    session = AsyncMock(ContextManagerMock)
+    session.__aenter__.return_value = context
+    session.request.return_value.__aenter__.return_value = response
+
+    return (session, context, response)
 
 
 @pytest.mark.asyncio
 async def test_async_mock_works(aiomock):
-    session, response = aiomock
+    session, context, response = aiomock
     with pytest.raises(aiohttp.ClientError):
         async with session:
             async with session.request(
@@ -31,5 +62,6 @@ async def test_async_mock_works(aiomock):
                 result = await _resp.json()
 
     session.request.assert_called_once()
+    context.request.assert_not_called()
     response.raise_for_status.assert_called_once()
     response.json.assert_not_called()
