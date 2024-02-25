5ec17696202a0260f9dac8edd3550f88f286ae4b
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-02-24 23:27:20 -0700

    add script to track raspberry pi cpu temps

---
+++ cpu_temp_graph.py
@@ -0,0 +1,34 @@
+import psutil
+import time
+import matplotlib.pyplot as plt
+
+# pip install matplotlib psutil
+
+plt.ion()
+x = []
+y = []
+
+
+def graph(temp):
+    y.append(temp)
+    x.append(time.time())
+    plt.clf()
+    plt.scatter(x, y)
+    plt.plot(x, y)
+    plt.draw()
+
+
+def get_cpu_temp():
+    temp = psutil.sensors_temperatures()["cpu_thermal"][0].current
+    return temp
+
+
+def main():
+    while True:
+        temp = get_cpu_temp()
+        graph(temp)
+        plt.pause(1)
+
+
+if __name__ == "__main__":
+    raise SystemExit(main())
