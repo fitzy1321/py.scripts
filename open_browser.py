d3cd4401fa8b2a6633308fc14e05b437f76653bd
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-07-22 13:49:42 -0600

    update open_browser module

--- open_browser.py
+++ open_browser.py
@@ -1,13 +1,31 @@
 import os
 import platform
 
-website_url = "https://google.com"
 
-os_system = platform.system()
+def platform_os_open_browser(url: str) -> None:
+    """OS awear way to open a web browser"""
+    os_system = platform.system()
 
-if "Windows" in os_system:
-    os.startfile(website_url)  # type: ignore
-elif "Darwin" in os_system:
-    os.system(f"open {website_url}")
-elif "Linux" in os_system:
-    os.system(f"xdg-open {website_url}")
+    if "Windows" in os_system:
+        os.startfile(url)  # type: ignore
+    elif "Darwin" in os_system:
+        os.system(f"open {url}")
+    elif "Linux" in os_system:
+        os.system(f"xdg-open {url}")
+
+
+def webbrowser_module(url: str) -> None:
+    """OS agnostic way to open a web browser"""
+    import webbrowser
+
+    webbrowser.open(url)
+
+
+def main(url: str):
+    platform_os_open_browser(url)
+    webbrowser_module(url)
+
+
+if __name__ == "__main__":
+    website_url = "https://google.com"
+    raise SystemExit(main(website_url))
