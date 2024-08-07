5bf03e3b8cd62c9ba039ffb56cc18089ef56e058
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-08-07 09:52:56 -0600

    trying to reduce time complexity

--- coin_change.py
+++ coin_change.py
@@ -3,6 +3,7 @@
 from functools import lru_cache
 from collections import defaultdict
 from timeit import timeit
+from typing import AbstractSet
 
 
 def _min_no_none(a, b):
@@ -51,7 +52,7 @@ def min_coins_memoized(target: int, coins: frozenset[int]):
     return answer
 
 
-def how_many_possibilities(target: int, coins: set[int]) -> int:
+def how_many_possibilities(target: int, coins: AbstractSet[int]) -> int:
     memo = defaultdict(lambda: 0)
 
     memo[0] = 1
@@ -68,25 +69,24 @@ def how_many_possibilities(target: int, coins: set[int]) -> int:
 
 def _dedup_combos(combo_list: list[list[int]]) -> list[list[int]]:
     sameses: dict[tuple[int, frozenset[int]], list[int]] = {}
-    for coins in combo_list:
-        key = (len(coins), frozenset(coins))
-        sameses[key] = coins
+    for combo in combo_list:
+        key = (len(combo), frozenset(combo))
+        sameses[key] = combo
     return list(sameses.values())
 
 
 def _dedup_combos_v2(combo_list: list[list[int]]) -> list[list[int]]:
-    sameses: set[tuple[int, frozenset[int]]] = set()
-    results = []
-    for coins in combo_list:
-        key = (len(coins), frozenset(coins))
+    sameses: dict[tuple[int, frozenset[int]], list[int]] = {}
+    for combo in combo_list:
+        key = (len(combo), frozenset(combo))
         if key in sameses:
             continue
-        sameses.add(key)
-        results.append(coins)
-    return results
+        sameses[key] = combo
+    return list(sameses.values())
 
 
-def full_list_possibilities(target: int, coins: set[int]) -> list[list[int]]:
+@lru_cache
+def full_list_possibilities(target: int, coins: frozenset[int]) -> list[list[int]]:
     def recurse(coins, target, current_combination, current_sum):
         if current_sum == target:
             result.append(list(current_combination))
@@ -105,7 +105,7 @@ def full_list_possibilities(target: int, coins: set[int]) -> list[list[int]]:
 
 def normalized_possibilities(
     target: int,
-    coins: set[int],
+    coins: frozenset[int],
 ) -> dict[int, list[list[int]]]:
     result = full_list_possibilities(target, coins)
     final = _dedup_combos_v2(result)
@@ -114,14 +114,14 @@ def normalized_possibilities(
 
 if __name__ == "__main__":
     target = 55
-    coin_set = {1, 5, 10, 25, 50}
+    coin_set = frozenset([1, 5, 10, 25, 50])
 
     print("~~ Coin Change Program ~~\n")
     print(f"Target Sum in Cents: {target}")
     print(f"Set of Coin values: {coin_set}")
     print()  # newline
     print(f"{min_coins(target, coin_set)=}")
-    print(f"{min_coins_memoized(target, frozenset(coin_set))=}")
+    print(f"{min_coins_memoized(target, coin_set)=}")
     print(f"{how_many_possibilities(target, coin_set)=}")
     print(f"{normalized_possibilities(target, coin_set)=}")
 
