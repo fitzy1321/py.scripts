65d52b67f424b9b107d90cfb963205e45e98c580
fitzypop <32967490+fitzypop@users.noreply.github.com>
2024-08-07 00:21:57 -0600

    tuples and frozensets FTW

--- coin_change.py
+++ coin_change.py
@@ -1,4 +1,4 @@
-# from functools import lru_cache
+from functools import lru_cache
 from collections import defaultdict
 
 
@@ -10,7 +10,7 @@ def _min_no_none(a, b):
     return min(a, b)
 
 
-def min_num_of_coins(target: int, coins: set[int]):
+def min_num_of_coins(target, coins):
     if not hasattr(min_num_of_coins, "memo"):
         min_num_of_coins.memo = {}
 
@@ -33,22 +33,22 @@ def min_num_of_coins(target: int, coins: set[int]):
     return answer
 
 
-# @lru_cache
-# def min_coins_memoized(target: int, coins: set[int]):
-#     answer = 0
-#     if target == 0:
-#         return answer
-#     else:
-#         for coin in coins:
-#             sub = target - coin
-#             if sub < 0:
-#                 continue
-#             answer = _min_no_none(answer, min_coins_memoized(sub, coins) + 1)
+@lru_cache
+def min_coins_memoized(target: int, coins: frozenset[int]):
+    if target == 0:
+        answer = 0
+    else:
+        answer = None
+        for coin in coins:
+            sub = target - coin
+            if sub < 0:
+                continue
+            answer = _min_no_none(answer, min_coins_memoized(sub, coins) + 1)
 
-#     return answer
+    return answer
 
 
-def how_many_ways(target, coins):
+def how_many_ways(target: int, coins: set[int]) -> int:
     memo = defaultdict(lambda: 0)
 
     memo[0] = 1
@@ -61,6 +61,16 @@ def how_many_ways(target, coins):
             memo[i] = memo[i] + memo[sub]
 
     return memo[target]
+
+
+def _undup_combos(combo_list: list[list[int]]) -> list[list[int]]:
+    # Tuples and Frozensets can be used as keys for dicts,
+    # because they are Hashable!
+    sameses: dict[tuple[int, frozenset[int]], list[int]] = {}
+    for coins in combo_list:
+        tuple_key = (len(coins), frozenset(coins))
+        sameses[tuple_key] = coins
+    return list(sameses.values())
 
 
 def all_coin_combs(target: int, coins: set[int]) -> dict[int, list[list[int]]]:
@@ -77,7 +87,8 @@ def all_coin_combs(target: int, coins: set[int]) -> dict[int, list[list[int]]]:
 
     result = []
     recurse(coins, target, [], 0)
-    return {len(result): sorted(result, key=len)}
+    final = _undup_combos(result)
+    return {len(final): final}
 
 
 if __name__ == "__main__":
@@ -89,10 +100,6 @@ if __name__ == "__main__":
     print(f"Set of Coin values: {s_coins}")
     print()  # newline
     print(f"{min_num_of_coins(target, s_coins)=}")
-    # print(f"{min_coins_memoized(target, s_coins)=}")
+    print(f"{min_coins_memoized(target, frozenset(s_coins))=}")
     print(f"{how_many_ways(target, s_coins)=}")
     print(f"{all_coin_combs(target, s_coins)=}")
-    print("\nNext Steps, how to deduplicate unorder coin combo lists?")
-    print(
-        "[1,1,2], [1,2,1], and [2,1,1] are technically the same combo in different orders"
-    )
